6639b42b46afbdf43e7e4679ff571b40be38b115
AnanasBln <57714769+AnanasBln@users.noreply.github.com>
2020-04-03 17:37:04 +0200

    first commit

--- blog.router.js
+++ blog.router.js
@@ -4,10 +4,25 @@ const express = require('express');
 const blogRouter = express.Router();
 
 blogRouter.get('/', (req, res)=> { 
-    const prenom = "Rui";
+    const prenom = "Anastasiya";
     res.render('index.pug', {prenom})
 });
-blogRouter.get('/admin', (req, res)=> res.send('Bienvenue'));
+blogRouter.get('/article', (req, res)=> { 
+
+    res.render('article.pug')
+});
+blogRouter.get('/admin', (req, res)=>{ 
+   
+    res.render('admin/admin.pug')
+});
+blogRouter.get('/admin/edit', (req, res)=>{ 
+   
+    res.render('admin/edit.pug')
+});
+blogRouter.get('/admin/write', (req, res)=>{ 
+   
+    res.render('admin/write.pug')
+});
 
 //Export l'objet Router crée
 module.exports = blogRouter;
